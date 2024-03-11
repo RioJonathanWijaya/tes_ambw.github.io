4e926ecaa65d706da56a1a250a12da1698b9e55e
RioJonathanWijaya <riojonathanwi@icloud.com>
2024-03-11 18:05:19 +0700

    href

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
-importScripts('/src/js/idb.js');
-importScripts('/src/js/utility.js');
+importScripts('src/js/idb.js');
+importScripts('src/js/utility.js');
 
 var CACHE_STATIC_NAME = 'static-v18';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 var STATIC_FILES = [
-  '/',
-  '/index.html',
-  '/offline.html',
-  '/src/js/app.js',
-  '/src/js/feed.js',
-  '/src/js/idb.js',
-  '/src/js/promise.js',
-  '/src/js/fetch.js',
-  '/src/js/material.min.js',
-  '/src/css/app.css',
-  '/src/css/feed.css',
-  '/src/images/main-image.jpg',
+  '',
+  'index.html',
+  'offline.html',
+  'src/js/app.js',
+  'src/js/feed.js',
+  'src/js/idb.js',
+  'src/js/promise.js',
+  'src/js/fetch.js',
+  'src/js/material.min.js',
+  'src/css/app.css',
+  'src/css/feed.css',
+  'src/images/main-image.jpg',
   'https://fonts.googleapis.com/css?family=Roboto:400,700',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
@@ -115,7 +115,7 @@ self.addEventListener('fetch', function (event) {
                 return caches.open(CACHE_STATIC_NAME)
                   .then(function (cache) {
                     if (event.request.headers.get('accept').includes('text/html')) {
-                      return cache.match('/offline.html');
+                      return cache.match('offline.html');
                     }
                   });
               });
